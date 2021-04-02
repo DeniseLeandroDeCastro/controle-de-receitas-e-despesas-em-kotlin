d653b5cfe4475c475c31af979e41f99d65164d5d
Denise Maria Leandro de Castro <denise.castro@pampa.compasso>
2021-04-02 15:10:08 -0300

    Cálculo: delegando responsabilidade

--- app/src/main/java/com/example/financask/model/Resumo.kt
+++ app/src/main/java/com/example/financask/model/Resumo.kt
@@ -3,8 +3,28 @@ package com.example.financask.model
 import android.view.View
 import java.math.BigDecimal
 
-class Resumo(private val view: View,
-             transacoes: List<Transacao>){
+class Resumo(private val transacoes: List<Transacao>) {
 
+    fun receita() : BigDecimal {
+        var totalReceita = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.RECEITA) {
+                totalReceita = totalReceita.plus(transacao.valor) //soma o total da receita com o valor da transação
+            }
+        }
+        return totalReceita
+    }
 
+    fun despesa() : BigDecimal {
+        var totalDespesa = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.DESPESA) {
+                totalDespesa = totalDespesa.plus(transacao.valor) //soma o total da despesa com o valor da transação
+            }
+        }
+        return totalDespesa
+    }
+    fun total() : BigDecimal {
+        return receita().subtract(despesa())
+    }
 }
--- app/src/main/java/com/example/financask/ui/ResumoView.kt
+++ app/src/main/java/com/example/financask/ui/ResumoView.kt
@@ -2,30 +2,39 @@ package com.example.financask.ui
 
 import android.view.View
 import com.example.financask.extension.formataParaBrasileiro
-import com.example.financask.model.Tipo
+import com.example.financask.model.Resumo
 import com.example.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
-import java.math.BigDecimal
 
 class ResumoView(private val view: View,
-                 private val transacoes: List<Transacao>) {
+                 transacoes: List<Transacao>) {
 
+    private val resumo: Resumo = Resumo(transacoes)
+
+    /**
+     * Função para adiconar os valores da receita
+     * Cálculo referente a esta função está  na
+     * classe Resumo.kt
+     */
     fun adicionaReceita() {
-        var totalReceita = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.RECEITA) {
-                totalReceita = totalReceita.plus(transacao.valor) //soma o total da receita com o valor da transação
-            }
-        }
-        view.resumo_card_receita.text = totalReceita.formataParaBrasileiro()
+        val totalReceita = resumo.receita()
+        view.resumo_card_receita.text = totalReceita
+                .formataParaBrasileiro()
     }
+    /**
+     * Função para adiconar os valores da despesa
+     * Cálculo referente a esta função está  na
+     * classe Resumo.kt
+     */
     fun adicionaDespesa() {
-        var totalReceita = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.DESPESA) {
-                totalReceita = totalReceita.plus(transacao.valor) //soma o total da receita com o valor da transação
-            }
-        }
-        view.resumo_card_despesa.text = totalReceita.formataParaBrasileiro()
+        val totalDespesa = resumo.despesa()
+        view.resumo_card_despesa.text = totalDespesa
+                .formataParaBrasileiro()
+    }
+    //Função para adicionar valores ao total
+    fun adicionaTotal() {
+        val total = resumo.total()
+        view.resumo_card_total.text = total
+                .formataParaBrasileiro()
     }
 }
